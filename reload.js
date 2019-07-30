05a5865e93f87daab3527558ba1b240289699470
axolib <52181632+axolib@users.noreply.github.com>
2019-07-29 18:45:42 -0700

    Update reload.js

--- reload.js
+++ reload.js
@@ -1,7 +1,11 @@
 var texts = [
   "AxoLib is a noob.",
-  "AxoLib was here",
-  "AxoLib.exe has stopped working..."
+  "AxoLib was here.",
+  "AxoLib is pretty epic ngl.",
+  "AxoLib has commited multiple felonies.",
+  "AxoLib is never wrong.",
+  "AxoLib has about 2 robux.",
+  "AxoLib.exe has stopped working...",
 ];
 
 document.getElementById("center").innerHTML = texts[Math.floor(Math.random()*texts.length)];;
