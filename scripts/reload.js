6f35d5ad2cc44a1f35582055d55c17bd4f224108
axolib <52181632+axolib@users.noreply.github.com>
2019-07-29 17:59:59 -0700

    Delete reload.js

--- scripts/reload.js
+++
@@ -1,6 +0,0 @@
-
-var phrases = ['AxoLib is a noob', 'AxoLib was here', 'AxoLib.exe has stopped responding...'];
-var randomPhrase = Math.floor(Math.random() * phrases.length);
-
-document.getElementById('center').innerHTML = phrases[randomPhrase];
-
