cd3c1f778047a0c75292fdc0d0ace990cb52abbd
axolib <52181632+axolib@users.noreply.github.com>
2019-07-29 17:58:16 -0700

    Update reload.js

--- scripts/reload.js
+++ scripts/reload.js
@@ -1,5 +1,6 @@
-(function newPhrase() {
-  var phrases = ['AxoLib is a noob', 'AxoLib was here', 'AxoLib.exe has stopped responding...'];
-  var randomPhrase = Math.floor(Math.random() * phrases.length);
-  document.getElementById('center').innerHTML = phrases[randomPhrase];
-})();
+
+var phrases = ['AxoLib is a noob', 'AxoLib was here', 'AxoLib.exe has stopped responding...'];
+var randomPhrase = Math.floor(Math.random() * phrases.length);
+
+document.getElementById('center').innerHTML = phrases[randomPhrase];
+
